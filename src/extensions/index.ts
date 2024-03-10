41477c86f609087c61f819d422a57ffdf0a5ddc9
Zoe <git@zoe-codez.xyz>
2024-03-10 10:16:49 -0500

    code transfer

---
+++ src/extensions/index.ts
@@ -0,0 +1,6 @@
+export * from "./binary-sensor.extension";
+export * from "./button.extension";
+export * from "./registry.extension";
+export * from "./scene.extension";
+export * from "./sensor.extension";
+export * from "./switch.extension";
