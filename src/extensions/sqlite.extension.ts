487babd0a110ebba78170e75661e863130e527ef
Zoe Codez <github@zoe-codez.tools>
2024-08-10 17:54:32 -0500

    case

--- src/extensions/sqlite.extension.ts
+++ src/extensions/sqlite.extension.ts
@@ -1,5 +1,5 @@
 import { is, TServiceParams } from "@digital-alchemy/core";
-import SqliteDriver, { Database } from "better-sqlite3";
+import SQLiteDriver, { Database } from "better-sqlite3";
 
 import { TSynapseId } from "../helpers";
 
@@ -36,7 +36,7 @@ export type HomeAssistantEntityRow = {
   application_name: string;
 };
 
-export type SynapseSqliteDriver = typeof SqliteDriver;
+export type SynapseSqliteDriver = typeof SQLiteDriver;
 
 export function SQLite({
   lifecycle,
@@ -48,7 +48,7 @@ export function SQLite({
 }: TServiceParams): SynapseSqlite {
   let database: Database;
   const application_name = internal.boot.application.name;
-  let Driver: SynapseSqliteDriver = SqliteDriver;
+  let Driver: SynapseSqliteDriver = SQLiteDriver;
 
   lifecycle.onPostConfig(() => {
     database = new Driver(config.synapse.SQLITE_DB);
