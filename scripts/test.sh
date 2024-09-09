8fa81589e3225ca2eb9f76fcd699ce5443a59877
Zoe Codez <github@zoe-codez.tools>
2024-09-08 19:34:12 -0500

    exit on fail

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-NODE_OPTIONS="$NODE_OPTIONS --experimental-vm-modules" npx jest --pass-with-no-tests --runInBand "$1"
+NODE_OPTIONS="$NODE_OPTIONS --experimental-vm-modules" npx jest --pass-with-no-tests --runInBand "$1" || exit 1
 rm jest_sqlite.db
